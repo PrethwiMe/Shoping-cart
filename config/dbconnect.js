1651f107a3c58483fffacbe5c10f53bc6fe1029c
PrethwiMe <prethwicoc@gmail.com>
2025-02-20 03:19:12 +0530

    Update dbconnect.js

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -5,7 +5,10 @@ const url = 'mongodb+srv://prethwicoc:aRAGL0wNX0I4Es16@cluster0.hreof.mongodb.ne
 const dbname = 'sample';
 let db;
 
-const client = new MongoClient(url)
+const client = new MongoClient(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 async function connectDB() {
 
