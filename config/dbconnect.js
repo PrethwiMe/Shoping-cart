752ead4993aa1a1f412aebc6c69e7320a36b7b7f
PrethwiMe <prethwicoc@gmail.com>
2025-02-20 03:14:59 +0530

    latest

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -1,11 +1,14 @@
 var { MongoClient } = require('mongodb')
 const express = require("express")
 
-const url = 'mongodb+srv://prethwicoc:aRAGL0wNX0I4Es16@cluster0.hreof.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const url = 'mongodb+srv://prethwicoc:aRAGL0wNX0I4Es16@cluster0.hreof.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tls=true';
 const dbname = 'sample';
 let db;
 
-const client = new MongoClient(url)
+const client = new MongoClient(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 async function connectDB() {
 
